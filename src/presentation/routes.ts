5e941b30d8b4829ec01d3400b093c85ffc4623d4
Aaron Jauregui <aaronjau21@gmail.com>
2024-07-21 02:22:37 -0500

    Create USE-STORE

---
+++ src/presentation/routes.ts
@@ -0,0 +1,12 @@
+import { Router } from 'express';
+
+
+export class AppRoutes {
+
+  public static get routes(): Router {
+    const router = Router();
+
+    return router;
+  }
+
+}
---
+++ src/presentation/server.ts
@@ -0,0 +1,36 @@
+import express, { Router } from 'express';
+
+interface Options {
+  port: number;
+  routes: Router;
+}
+
+export class Server {
+
+  // TODO verificar si funciona como privada el app
+  private readonly app = express();
+  private serverListener?: any;
+  private readonly port: number;
+  private readonly routes: Router;
+
+  constructor( options: Options ) {
+    const { port, routes } = options;
+
+    this.port = port;
+    this.routes = routes;
+  }
+
+  async start() {
+    this.app.use( express.json() );
+    this.app.use( express.urlencoded( { extended: true } ) );
+
+    this.app.use( this.routes );
+
+    this.serverListener = this.app.listen( this.port, () => console.log( `Server running on port ${ this.port }` ) );
+  }
+
+  public close() {
+    this.serverListener?.close();
+  }
+
+}
