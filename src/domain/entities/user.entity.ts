61cf7550f36324108c771504ea0cdc2760071de8
Aaron Jauregui <aaronjau21@gmail.com>
2024-07-21 11:24:59 -0500

    Created CustomErrors, User Model, User Entities

---
+++ src/domain/entities/user.entity.ts
@@ -0,0 +1,32 @@
+import { CustomError } from '../errors/custom.error';
+
+export class UserEntity {
+
+  constructor(
+    public id: string,
+    public name: string,
+    public email: string,
+    public emilValidated: boolean,
+    public password: string,
+    public role: string[],
+    public img?: string,
+  ) { }
+
+  static fromObject( object: { [ key: string ]: any; } ) {
+    const { id, _id, name, email, emilValidated, password, role, img } = object;
+
+    if ( !_id && id ) {
+      throw CustomError.badRequest( 'Missing id' );
+    }
+
+    if ( !name ) throw CustomError.badRequest( 'Missing name' );
+    if ( !email ) throw CustomError.badRequest( 'Missing email' );
+    if ( emilValidated === undefined ) throw CustomError.badRequest( 'Missing emilValidated' );
+    if ( !password ) throw CustomError.badRequest( 'Missing password' );
+    if ( !role ) throw CustomError.badRequest( 'Missing role' );
+
+    return new UserEntity( _id || id, name, email, emilValidated, password, role, img );
+
+  }
+
+}
